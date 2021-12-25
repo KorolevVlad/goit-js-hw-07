50438eaf744aad72840c19932faf05774b39b3fd
Korolev Vlad <korolev.v.s@bk.ru>
2021-12-25 16:00:57 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImageAll = document.querySelector(".gallery");
+
+const imageOfGallery = createImageOfGalleryEl(galleryItems);
+
+galleryImageAll.insertAdjacentHTML("beforeend", imageOfGallery);
+
+function createImageOfGalleryEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      title="${description}"
+    />
+  </a>
+`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", () => {
+  captionsData: "title";
+  captionDelay: 250;
+});
